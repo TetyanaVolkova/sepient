a7384fff770488e2611e32281351dde18277813c
Tetyana <tatankavolkova@gmail.com>
2017-03-20 04:55:56 -0400

    Added styles.

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import {Row, Col, Grid } from 'react-bootstrap';
+import {Row, Col, Grid, ButtonToolbar, MenuItem, Dropdown, Button, FormGroup, FormControl, InputGroup, DropdownButton } from 'react-bootstrap';
 import ProdImage from '../../public/images/tough-mojo-jacket-img1.png';
+import NumericInput from 'react-numeric-input';
 
 class Products extends Component {
   render() {
@@ -16,12 +17,69 @@ class Products extends Component {
             <p>SCHOELLER MICROFIBER SHELL</p>
             <p>DIAGONAL QUILTING</p>
             <p>MICROFIBER RIPSTOP LINER</p>
-            <p>300 GRAMS OF PRIMALOFT ONE INSULATION</p>
-            <p>300 GRAMS OF PRIMALOFT ONE INSULATION</p>
-            <p>300 GRAMS OF PRIMALOFT ONE INSULATION</p>
-            <p>300 GRAMS OF PRIMALOFT ONE INSULATION</p>
-            <p>300 GRAMS OF PRIMALOFT ONE INSULATION</p>
-            <p>300 GRAMS OF PRIMALOFT ONE INSULATION</p>
+            <p>ZIPPER GARAGE AT COLLAR</p>
+            <p>ANCHORED BOTTOM-HEM ADJUSTERS</p>
+            <p>3-IN-1 FRONT POCKETS</p>
+            <p>ZIPPERED CHEST POCKET</p>
+            <p>TWO INTERIOR ZIPPERD POCKETS</p>
+            <p>MADE IN THE PHILIPPINES</p>
+
+            <ButtonToolbar>
+
+            <Dropdown>
+              <Button className="dropdown-custom">
+                Select Size
+              </Button>
+              <Dropdown.Toggle className="dropdown-custom"/>
+              <Dropdown.Menu>
+                <MenuItem eventKey="1">XSmall</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="2">Small</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="3">Medium</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="4">Large</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey="4">XLarge</MenuItem>
+              </Dropdown.Menu>
+            </Dropdown>
+
+              <Dropdown>
+                <Button className="dropdown-custom">
+                  Select Color
+                </Button>
+                <Dropdown.Toggle className="dropdown-custom"/>
+                <Dropdown.Menu>
+                  <MenuItem eventKey="1">White</MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey="2">Black</MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey="3">Red</MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey="4">Blue</MenuItem>
+                </Dropdown.Menu>
+              </Dropdown>
+
+            </ButtonToolbar>
+            <ButtonToolbar>
+              <Dropdown>
+                <Button className="numeric">
+                  0
+                </Button>
+                <Dropdown.Toggle className="dropdown-custom"/>
+                <Dropdown.Menu>
+                  <MenuItem eventKey="1">1</MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey="2">2</MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey="3">3</MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey="4">4</MenuItem>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Button className="add-to-cart-btn">Add To Cart</Button>
+            </ButtonToolbar>
+
           </Col>
         </Row>
       </Grid>
--- src/Components/Submenu.js
+++ src/Components/Submenu.js
@@ -9,38 +9,48 @@ class Submenu extends Component {
           <Nav bsStyle="tabs" activeKey="1" onSelect={this.handleSelect}>
           <NavDropdown eventKey="4" title="Camping" id="nav-dropdown">
             <MenuItem eventKey="4.1">Action</MenuItem>
+            <MenuItem divider />
             <MenuItem eventKey="4.2">Another action</MenuItem>
+            <MenuItem divider />
             <MenuItem eventKey="4.3">Something else here</MenuItem>
             <MenuItem divider />
-            <MenuItem eventKey="4.4">Separated link</MenuItem>
+            <MenuItem eventKey="4.4">And more</MenuItem>
           </NavDropdown>
             <NavDropdown eventKey="4" title="Clothing" id="nav-dropdown">
-              <MenuItem eventKey="4.1">Action</MenuItem>
-              <MenuItem eventKey="4.2">Another action</MenuItem>
-              <MenuItem eventKey="4.3">Something else here</MenuItem>
+              <MenuItem active eventKey="4.1">Outerwear</MenuItem>
               <MenuItem divider />
-              <MenuItem eventKey="4.4">Separated link</MenuItem>
+              <MenuItem eventKey="4.2">Hiking Shoes / Boots</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey="4.3">Gloves</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey="4.4">Hats</MenuItem>
             </NavDropdown>
             <NavDropdown eventKey="4" title="Fishing" id="nav-dropdown">
               <MenuItem eventKey="4.1">Action</MenuItem>
+              <MenuItem divider />
               <MenuItem eventKey="4.2">Another action</MenuItem>
+              <MenuItem divider />
               <MenuItem eventKey="4.3">Something else here</MenuItem>
               <MenuItem divider />
-              <MenuItem eventKey="4.4">Separated link</MenuItem>
+              <MenuItem eventKey="4.4">And more</MenuItem>
             </NavDropdown>
             <NavDropdown eventKey="4" title="Hiking" id="nav-dropdown">
               <MenuItem eventKey="4.1">Action</MenuItem>
+              <MenuItem divider />
               <MenuItem eventKey="4.2">Another action</MenuItem>
+              <MenuItem divider />
               <MenuItem eventKey="4.3">Something else here</MenuItem>
               <MenuItem divider />
-              <MenuItem eventKey="4.4">Separated link</MenuItem>
+              <MenuItem eventKey="4.4">And more</MenuItem>
             </NavDropdown>
             <NavDropdown eventKey="4" title="Biking" id="nav-dropdown">
               <MenuItem eventKey="4.1">Action</MenuItem>
+              <MenuItem divider />
               <MenuItem eventKey="4.2">Another action</MenuItem>
+              <MenuItem divider />
               <MenuItem eventKey="4.3">Something else here</MenuItem>
               <MenuItem divider />
-              <MenuItem eventKey="4.4">Separated link</MenuItem>
+              <MenuItem eventKey="4.4">And more</MenuItem>
             </NavDropdown>
           </Nav>
         </Row>
